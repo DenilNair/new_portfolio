4fd61d2b3c96f4bcc4a203799f95ed9c10c392c2
DenilNair <denil007nair@gmail.com>
2023-11-26 21:02:09 +0530

    loading elements based on loop and added projects under services

--- src/component/service/service.js
+++ src/component/service/service.js
@@ -1,72 +1,80 @@
-import React from 'react'
-import './service.css';
-import Card from '../cards/card';
+import React from "react";
+import "./service.css";
+import Card from "../cards/card";
 const Service = () => {
+  const technologiesList = [
+    { key: "Angular", value: process.env.PUBLIC_URL + "/img/angular.png" },
+    { key: "Java", value: process.env.PUBLIC_URL + "/img/java.jpg" },
+    { key: "Node JS", value: process.env.PUBLIC_URL + "/img/node.png" },
+    {
+      key: "Oracle Database",
+      value: process.env.PUBLIC_URL + "/img/oracle.png",
+    },
+    { key: "React", value: process.env.PUBLIC_URL + "/img/react.png" },
+    { key: "Android", value: process.env.PUBLIC_URL + "/img/android.jpg" },
+    {
+      key: "Chrome Extension",
+      value: process.env.PUBLIC_URL + "/img/extension.jpg",
+    },
+    { key: "Javascript", value: process.env.PUBLIC_URL + "/img/js.png" },
+    {
+      key: "Springboot",
+      value: process.env.PUBLIC_URL + "/img/springboot.png",
+    },
+    { key: "Ionic", value: process.env.PUBLIC_URL + "/img/ionic.png" },
+  ];
   return (
-    <div className='mainService'>
+    <div className="mainService">
       <div>service</div>
-      <div><h4>Technologies</h4><div>
-        <div >
-
-          <div class="outer-grid">
-
-
-            <div class="grid-item">Previous</div>
-            <div class="grid-item">
-              <div class="inner-grid">
-                <div class="grid-item"><Card passedData="Angular" passedImage={process.env.PUBLIC_URL + "/img/angular.png"} /></div>
-                <div class="grid-item"><Card passedData="Java" passedImage={process.env.PUBLIC_URL + "/img/java.jpg"} /></div>
-                <div class="grid-item"><Card passedData="Node JS" passedImage={process.env.PUBLIC_URL + "/img/node.png"} />
+      <div>
+        <h4>Technologies</h4>
+        <div>
+          <div className="gridClass">
+            <div class="outer-grid">
+              <div class="grid-item">Previous</div>
+              <div class="grid-item">
+                <div class="inner-grid">
+                {technologiesList.map((pair, index) => (
+                    <TechnologiesPair key={index} pair={pair} />
+                  ))}
                 </div>
-                <div class="grid-item"><Card passedData="Oracle Database" passedImage={process.env.PUBLIC_URL + "/img/oracle.png"} />
-                </div>
-                <div class="grid-item"><Card passedData="React" passedImage={process.env.PUBLIC_URL + "/img/react.png"} />
-                </div>
-                <div class="grid-item"><Card passedData="Android" passedImage={process.env.PUBLIC_URL + "/img/android.jpg"} />
-                </div>
-                <div class="grid-item"><Card passedData="chrome-extension" passedImage={process.env.PUBLIC_URL + "/img/extension.jpg"} />
-                </div>
-                <div class="grid-item"><Card passedData="Javascript" passedImage={process.env.PUBLIC_URL + "/img/js.png"} />
-                </div>
-                <div class="grid-item"><Card passedData="spring-boot" passedImage={process.env.PUBLIC_URL + "/img/springboot.png"} />
-                </div>
-                <div class="grid-item"> <Card passedData="ionic" passedImage={process.env.PUBLIC_URL + "/img/ionic.png"} />
-                </div>
-
               </div>
-            </div>
 
-            <div class="grid-item">Next</div>
-          </div>
-
-
-          <div ><button className='controls'>Previous</button>
-          </div>
-          <div className='slider-container'>
-            <div className='slider'>
-              <Card passedData="Angular" passedImage={process.env.PUBLIC_URL + "/img/angular.png"} />
-
-              <Card passedData="Java" passedImage={process.env.PUBLIC_URL + "/img/java.jpg"} />
-              <Card passedData="Node JS" passedImage={process.env.PUBLIC_URL + "/img/node.png"} />
-              <Card passedData="Oracle Database" passedImage={process.env.PUBLIC_URL + "/img/oracle.png"} />
-              <Card passedData="React" passedImage={process.env.PUBLIC_URL + "/img/react.png"} />
-              <Card passedData="spring-boot" passedImage={process.env.PUBLIC_URL + "/img/springboot.png"} />
-              <Card passedData="ionic" passedImage={process.env.PUBLIC_URL + "/img/ionic.png"} />
-              <Card passedData="Android" passedImage={process.env.PUBLIC_URL + "/img/android.jpg"} />
-              <Card passedData="chrome-extension" passedImage={process.env.PUBLIC_URL + "/img/extension.jpg"} />
-              <Card passedData="Javascript" passedImage={process.env.PUBLIC_URL + "/img/js.png"} />
+              <div class="grid-item">Next</div>
             </div>
           </div>
-          <div><button className='controls'>Next</button></div>
-
         </div>
-
-      </div>
       </div>
 
+      <div>
+        <h4>Projects</h4>
+        <div>
+          <div className="gridClass">
+            <div class="outer-grid">
+              <div class="grid-item">Previous</div>
+              <div class="grid-item">
+                <div class="inner-grid">
+                  {technologiesList.map((pair, index) => (
+                    <TechnologiesPair key={index} pair={pair} />
+                  ))}
+                </div>
+              </div>
+
+              <div class="grid-item">Next</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  );
+};
 
-  )
-}
+const TechnologiesPair = ({ pair }) => {
+  return (
+    <div class="grid-item">
+      <Card passedData={pair.key} passedImage={pair.value} />
+    </div>
+  );
+};
 
-export default Service
+export default Service;
